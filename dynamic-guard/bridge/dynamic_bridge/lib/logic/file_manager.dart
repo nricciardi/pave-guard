1e51cab0336cc5cdf50465b7fd6dd987131cc900
FGaragnani <filippogaragnani1@gmail.com>
2024-10-29 16:01:11 +0100

    File Manager Dart

---
+++ dynamic-guard/bridge/dynamic_bridge/lib/logic/file_manager.dart
@@ -0,0 +1,49 @@
+import 'dart:developer';
+
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'env_manager.dart';
+
+class FileManager {
+
+  // The name of the file from which to read
+  final String fileName;
+
+  FileManager(this.fileName);
+
+  // Gets the permanent documents directory
+  Future<String> get _docsPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  // Loads the file
+  Future<File> get _localFile async {
+    final path = await _docsPath;
+    if(EnvManager.isDebugMode()){
+      log("Getting file $path/$fileName");
+    }
+    return File('$path/$fileName');
+  }
+
+  // Reads from the file
+  Future<String> readFileContents() async {
+  
+    final file = await _localFile;
+
+    // Reads the file
+    final String contents = await file.readAsString();
+
+    return contents;
+  }
+
+  // Writes onto the file
+  void writeCounter(String counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    file.writeAsString(counter);
+  }
+
+}
