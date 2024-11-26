574a907914449186a877ab1177fec4fd3a00073d
FGaragnani <filippogaragnani1@gmail.com>
2024-11-26 19:58:25 +0100

    Inizio dell'HoleDetector

--- dynamic-guard/bridge/dynamic_bridge/lib/logic/hole_detector.dart
+++ dynamic-guard/bridge/dynamic_bridge/lib/logic/hole_detector.dart
@@ -1,8 +1,20 @@
 import 'package:camera/camera.dart';
+import 'package:flutter_vision/flutter_vision.dart';
 
 class HoleDetector {
 
-  static final List<double> weights = [];
+  FlutterVision flutterVision = FlutterVision();
+
+  Future<void> initialize() async {
+    await flutterVision.loadYoloModel(
+      modelPath: "assets/model.tflite", 
+      labels: "assets/labels.txt", 
+      modelVersion: "yolov8",
+      quantization: false,
+      numThreads: 1,
+      useGpu: false
+      );
+  }
 
   static bool isHole(XFile file){
 
--- dynamic-guard/bridge/yolo_model/model_train.py
+++ dynamic-guard/bridge/yolo_model/model_train.py
@@ -3,10 +3,10 @@ from ultralytics import YOLO
 model = YOLO('dynamic-guard/bridge/yolo_model/yolov8n.pt')
 
 model.train(
-    data="C:/Users/filip/Desktop/Universita/Anno IV - Semestre I/IOT/pave-guard/dynamic-guard/bridge/yolo_model/datasets/dataset.yaml",
+    data="datasets/dataset.yaml",
     epochs=50,
     imgsz=640,
-    batch=16,
+    batch=32,
 )
 
-model.val(data="C:/Users/filip/Desktop/Universita/Anno IV - Semestre I/IOT/pave-guard/dynamic-guard/bridge/yolo_model/datasets/dataset.yaml")
+model.val(data="datasets/dataset.yaml")
