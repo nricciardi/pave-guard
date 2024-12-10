7a18d0db674c1ae4ce49a02695c34b63fcea0e0c
FGaragnani <filippogaragnani1@gmail.com>
2024-12-10 16:52:36 +0100

    Update login_screen.dart

--- dashboard/pave_guard_dashboard/lib/screens/login_screen.dart
+++ dashboard/pave_guard_dashboard/lib/screens/login_screen.dart
@@ -1,34 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
+import 'package:responsive_admin_dashboard/database/query_manager.dart';
 import 'package:responsive_admin_dashboard/screens/dash_board_screen.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
 
+  LoginScreen({super.key});
+
+  final LoginManager loginManager = LoginManager();
   Duration get loginTime => const Duration(milliseconds: 2250);
 
-  Future<String?> _authUser(LoginData data) {
-    Map<String, String> users = {"marco": "pini"};
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
-  }
+  Future<String?> _authUser(LoginData data) async {
 
-  Future<String> _recoverPassword(String name) {
-    Map<String, String> users = {"marco": "pini"};
-    debugPrint('Name: $name');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return 'User not exists';
-      }
-      return "";
-    });
+    LoginManager loginManager = LoginManager();
+    try{ await loginManager.sendQuery(data);  } 
+    catch(e){ return "Connection error";      }
+
+    return null;
+
   }
 
   @override
@@ -39,10 +28,10 @@ class LoginScreen extends StatelessWidget {
       onSignup: (_) => Future.delayed(loginTime),
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) => const DashBoardScreen(),
+          builder: (context) => DashBoardScreen(token: loginManager.getToken()),
         ));
       },
-      onRecoverPassword: _recoverPassword,
+      onRecoverPassword: (_) => Future.delayed(loginTime),
       hideForgotPasswordButton: true,
       hideSignUpButton: true,
     );
