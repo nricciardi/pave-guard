9ec9fed33970ebe779934dc1e41b756cbe7cbffa
FGaragnani <filippogaragnani1@gmail.com>
2024-12-14 01:58:28 +0100

    Updated dotenv

--- dashboard/pave_guard_dashboard/lib/main.dart
+++ dashboard/pave_guard_dashboard/lib/main.dart
@@ -4,8 +4,10 @@ import 'screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
