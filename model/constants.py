a9b3c38843898560f636749e6ef2d70c962b0ecf
Nicola Ricciardi <ricciardincl@gmail.com>
2024-12-15 20:34:00 +0100

    Added Preprocessor scaffold

---
+++ model/constants.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class RawFeatureName(Enum):
+    RAINFALL = "rainfall"
+    TEMPERATURE = "temperature"
+    HUMIDITY = "humidity"
+    CRACK = "crack"
+    POTHOLE = "pothole"
+    TRANSIT_VELOCITY = "transit_velocity"
+    TRANSIT_LENGTH = "transit_length"
+    TRANSIT_TIME = "transit_time"
+
+
+
+class FeatureName(Enum):
+    SUBZERO_TEMPERATURE_MEAN = "subzero_temperature_mean"
+    TEMPERATURE_MEAN = "temperature_mean"
+    HUMIDITY_MEAN = "humidity_mean"
+    RAINFALL_QUANTITY = "rainfall_quantity"
+    STORM_TOTAL = "storm_total"
+    DELTA_TEMPERATURE = "delta_temperature"
+    HEAVY_VEHICLES_TRANSIT_TOTAL = "heavy_vehicles_transit_total"
+    TRANSIT_TOTAL = "transit_total"
+    TRANSIT_DURING_RAINFALL = "transit_during_rainfall"
+    HEAVY_VEHICLES_TRANSIT_DURING_RAINFALL = "heavy_vehicles_transit_during_rainfall"
+    DAYS = "days"
+    POTHOLE_SEVERITY = "pothole_severity"
+    CRACK_SEVERITY = "crack_severity"
+
+
+class TargetName(Enum):
+    POTHOLE_SEVERITY = "pothole_severity_target"
+    CRACK_SEVERITY = "crack_severity_target"
+    
+    
+    
--- model/preprocess/preprocessor.py
+++ model/preprocess/preprocessor.py
@@ -1,9 +1,31 @@
 import pandas as pd
 import numpy as np
 
+from constants import FeatureName, RawFeatureName
+
 
 class Preprocessor:
 
+    def subzeroTemperatureMean(self, temperature: np.ndarray) -> float:
+        return temperature[temperature < 0].mean(axis=0)
 
-    def process(self, dataset: pd.DataFrame) -> pd.DataFrame:
-        pass
+    def temperatureMean(self, temperature: np.ndarray) -> float:
+        return temperature.mean(axis=0)
+
+    def humidityMean(self, humidity: np.ndarray) -> float:
+        return humidity.mean(axis=0)
+
+    def rainTotal(self, rain: np.ndarray) -> float:
+        return rain.sum(axis=0)
+
+
+    def process(self, raw_dataset: pd.DataFrame) -> pd.DataFrame:
+
+        dataset = pd.DataFrame()
+
+        dataset[FeatureName.TEMPERATURE_MEAN] = self.temperatureMean(raw_dataset[RawFeatureName.TEMPERATURE])
+        dataset[FeatureName.HUMIDITY_MEAN] = self.temperatureMean(raw_dataset[RawFeatureName.HUMIDITY])
+
+        # TODO
+
+        return dataset
