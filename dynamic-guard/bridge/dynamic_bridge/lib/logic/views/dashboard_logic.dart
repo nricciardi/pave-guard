ef531fc149e81f92b02147711be3b93c8ca1965b
FGaragnani <filippogaragnani1@gmail.com>
2025-01-11 16:19:38 +0100

    Test feature removed

--- dynamic-guard/bridge/dynamic_bridge/lib/logic/views/dashboard_logic.dart
+++ dynamic-guard/bridge/dynamic_bridge/lib/logic/views/dashboard_logic.dart
@@ -32,7 +32,7 @@ class DashboardLogic {
 
   SerialInterface? serialInterface;
   HoleDetector? holeDetector;
-  
+
   DeviceData deviceData;
   DashboardLogic(this.deviceData);
 
@@ -40,11 +40,11 @@ class DashboardLogic {
     if (cameraController is CameraController) {
       // built-in
       CameraController toUse = cameraController as CameraController;
-      try{
+      try {
         return await toUse.takePicture();
-        }catch(e){
-          return null;
-        }
+      } catch (e) {
+        return null;
+      }
     } else {
       // USB cam
       PhotoCollector toUse = cameraController as PhotoCollector;
@@ -62,16 +62,17 @@ class DashboardLogic {
 
   void collectAndSendTelemetries() async {
     serialInterface ??= SerialInterface();
-    if(serialInterface!.isInitialized == true){ return; }
+    if (serialInterface!.isInitialized == true) {
+      return;
+    }
     try {
       serialInterface = SerialInterface();
       await serialInterface!.initialize();
       Transaction<String> transaction = Transaction.stringTerminated(
-          serialInterface!.port!.inputStream!, Uint8List.fromList([10, 10])
-          );
+          serialInterface!.port!.inputStream!, Uint8List.fromList([10, 10]));
       transaction.stream.listen((String data) async {
         for (String line in data.split("\n")) {
-          if(line.isNotEmpty){
+          if (line.isNotEmpty) {
             serialInterface!.manageSerialLine(line.trim());
           }
         }
@@ -90,20 +91,22 @@ class DashboardLogic {
     _timer = Timer.periodic(Duration(seconds: interval), (timer) async {
       // The photo collection
       XFile? file = await takePicture();
-      if(file == null){return;}
+      if (file == null) {
+        return;
+      }
       int holeSeverity = await holeDetector!.isHole(file);
       if (holeSeverity > 0) {
         RoadPotholeTelemetryQuery telemetryQuery = RoadPotholeTelemetryQuery();
         GPSData? gpsData;
-        try{ 
+        try {
           gpsData = serialInterface!.vibrationManager.getGpsData();
         } catch (e) {
           return;
         }
-        telemetryQuery.sendQuery( 
-          HoleSendableData(holeSeverity, deviceData, gpsData.latitude, gpsData.longitude, await nominatimManager.sendQuery(gpsData)), 
-          token: await TokenManager.getToken()
-        );
+        telemetryQuery.sendQuery(
+            HoleSendableData(holeSeverity, deviceData, gpsData.latitude,
+                gpsData.longitude, await nominatimManager.sendQuery(gpsData)),
+            token: await TokenManager.getToken());
       }
     });
   }
@@ -111,15 +114,33 @@ class DashboardLogic {
   Future<List<Widget>> dashboardCenterChildren() async {
     List<Widget> children = [];
     SettingsLogic settingsLogic = SettingsLogic();
-    if(holeDetector == null){
+    if (holeDetector == null) {
       holeDetector = HoleDetector();
       await holeDetector!.initialize();
     }
 
-    children.add(const Text(
-      'Dashboard',
-      style: TextStyle(fontSize: 24),
-    ));
+    children.add(
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.shield,
+              size: 30,
+              color: Colors.blueAccent), // A placeholder for a custom logo
+          const SizedBox(width: 10),
+          Text(
+            'PaveGuard Dashboard',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              foreground: Paint()
+                ..shader = const LinearGradient(
+                  colors: <Color>[Colors.blue, Colors.purple],
+                ).createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0)),
+            ),
+          ),
+        ],
+      ),
+    );
 
     if (await settingsLogic.isCameraExt()) {
       // The camera is external
@@ -127,22 +148,82 @@ class DashboardLogic {
       try {
         UVCCameraController uvcCameraController =
             await PhotoCollector.openExternalCamera();
-        children.add(UVCCameraView(
-          cameraController: uvcCameraController,
-          width: 480, height: 480
-        ));
-        children.add(const Text(
-          'External Camera loaded and working.',
-          style: TextStyle(fontSize: 24, color: Colors.green),
-          textAlign: TextAlign.center,
-        ));
+        children.add(
+          Column(
+            children: [
+              const Text(
+                'Live Camera Feed',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3), // Shadow position
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: UVCCameraView(
+                    cameraController: uvcCameraController,
+                    width: 480,
+                    height: 480,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+        children.add(
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.check_circle, color: Colors.green, size: 28),
+              SizedBox(width: 10),
+              Text(
+                'External Camera loaded and working.',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        );
         uvcCameraController.msgCallback;
         cameraWorking = true;
       } catch (e) {
-        children.add(const Text(
-          'External Camera not loading!',
-          style: TextStyle(fontSize: 24, color: Colors.red),
-        ));
+        children.add(
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.error, color: Colors.red, size: 28), // Icon for error
+              SizedBox(width: 10),
+              Text(
+                'External Camera not loading!',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        );
       }
     } else {
       // The camera is internal
