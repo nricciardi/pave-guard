a4d1e398128e05cd88d92f4cd381b0e89de46221
FGaragnani <filippogaragnani1@gmail.com>
2025-01-23 20:33:29 +0100

    Modificato come ci siamo detti
    NB: Ora la funzione da invocare è la process_and_partition()

--- model/pgmodel/model/model.py
+++ model/pgmodel/model/model.py
@@ -1,19 +1,13 @@
 from pgmodel.dataset.dataset_generator import DatasetGenerator
 from pgmodel.preprocess.preprocessor import Preprocessor
 
-
-
 if __name__ == '__main__':
 
-    input_dir = "/home/nricciardi/Repositories/pave-guard/model/pgmodel/dataset/data"
+    # input_dir = "/home/nricciardi/Repositories/pave-guard/model/pgmodel/dataset/data"
+    input_dir = "pgmodel/dataset/data"
 
     df = DatasetGenerator.csv_to_dataframe(input_dir)
 
-
-
     preprocessor = Preprocessor()
-    dataset = preprocessor.process(
-        df
-    )
-
+    dataset = preprocessor.partition_and_process(df)
     print(dataset.head())
