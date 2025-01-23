eeae6043c9ee773749d6ce99bb4fff6724faa60a
Nicola Ricciardi <ricciardincl@gmail.com>
2025-01-23 22:30:03 +0100

    Model scaffold

--- model/pgmodel/model/model.py
+++ model/pgmodel/model/model.py
@@ -1,5 +1,29 @@
 from pgmodel.dataset.dataset_generator import DatasetGenerator
 from pgmodel.preprocess.preprocessor import Preprocessor
+import pandas as pd
+from sklearn.base import BaseEstimator
+from sklearn.tree import DecisionTreeClassifier
+
+
+
+class PaveGuardModel:
+
+    def __init__(self, crack_model: BaseEstimator, pothole_model: BaseEstimator):
+        self.crack_model = crack_model
+        self.pothole_model = pothole_model
+
+    def fit(self, X: pd.DataFrame, Y_crack: pd.Series, Y_pothole: pd.Series):
+        self.crack_model.fit(X, Y_crack)
+        self.pothole_model.fit(X, Y_pothole)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -17,3 +41,6 @@ if __name__ == '__main__':
     )
 
     print(dataset.head())
+
+
+    model = PaveGuardModel(DecisionTreeClassifier(), DecisionTreeClassifier())
