85a7347715429a3cc15dd259f150e2d5bf8ceb6f
FGaragnani <filippogaragnani1@gmail.com>
2025-01-30 14:16:10 +0100

    Aggiunta

--- model/pgmodel/preprocess/preprocessor.py
+++ model/pgmodel/preprocess/preprocessor.py
@@ -164,12 +164,10 @@ class Preprocessor:
                 raw_dataset.at[first_occurrence_index, RawFeatureName.POTHOLE.value] = mean_crack_pothole
 
         raw_dataset = raw_dataset.dropna(how='all')
-        raw_dataset = raw_dataset.copy()
         # Add maintenance info
-        raw_dataset['maintenance'] = raw_dataset.apply(
-            lambda row: 1 if any(
-            maintenance['date'].date() == row.name.date() for maintenance in maintenances
-            ) else 0, axis=1
+        maintenance_dates = {maintenance['date'].date() for maintenance in maintenances}
+        raw_dataset = raw_dataset.assign(
+            maintenance=lambda df: df.index.date.isin(maintenance_dates).astype(int)
         )
 
         return self.partition_and_process(raw_dataset, index_list_crack, index_list_pothole, consecutive_measures_only=consecutive_measures_only)
